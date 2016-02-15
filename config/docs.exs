1f999349f12da090067d58b3e7653506411bc370
cncgl <concigel@gmail.com>
2016-02-15 11:30:13 +0900

    add ci

---
+++ config/docs.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Set a higher stacktrace during test
+config :phoenix, :stacktrace_depth, 20
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "sample_test",
+  pool: Ecto.Adapters.SQL.Sandbox
